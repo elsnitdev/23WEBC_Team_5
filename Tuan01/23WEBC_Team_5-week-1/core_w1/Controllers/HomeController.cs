383d2941e8c1ae1b9700958a2fa2f06582221dc6
Hyu0582 <hkhuy2208@gmail.com>
2025-09-21 21:35:36 +0700

    câu 2 - Thêm section "AppSettings" chứa MaxSizeFile và ListBannedIPs - Thêm Action TestAppSettings - HomeController: in MaxSizeFile và ListBannedIPs ra console

---
+++ Tuan01/23WEBC_Team_5-week-1/core_w1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using core_w1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace core_w1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult TestAppSettings()
+        {
+            long maxFileSize = _configuration.GetValue<long>("AppSettings:MaxFileSize");
+            Console.WriteLine("Max file size: " + maxFileSize);
+            string[] listBannedIPs = _configuration.GetSection("AppSettings:ListBannedIPs").Get<string[]>();
+            Console.WriteLine("List banned IPs: " + string.Join(", ", listBannedIPs));
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
