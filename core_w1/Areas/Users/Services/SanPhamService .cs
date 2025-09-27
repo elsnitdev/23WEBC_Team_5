f6c154e6db610afe013b5dbd48205aeccd2e2f9d
Thương <thuongdozoz25@gmail.com>
2025-09-27 21:28:59 +0700

    Thêm 2 file ISanPhamService và SanPhamService ,(DI chứa sản phẩm)

---
+++ core_w1/Areas/Users/Services/SanPhamService .cs
@@ -0,0 +1,19 @@
+﻿using core_w2.Areas.Users.Models;
+using System.Text.Json;
+namespace core_w2.Areas.Users.Services
+{
+    public class SanPhamService : ISanPhamService
+    {
+        private readonly List<SanPham> _dsSanPham;
+        public SanPhamService()
+        {
+            // Giả lập dữ liệu ban đầu
+            _dsSanPham = new List<SanPham>
+            {
+                new SanPham { MaSP = 1, TenSP = "Laptop", DonGia = 15000000, DonGiaKhuyenMai = 14000000, HinhAnh="laptop.jpg", MoTa="Laptop gaming", LoaiSP="Điện tử" },
+                new SanPham { MaSP = 2, TenSP = "Điện thoại", DonGia = 8000000, DonGiaKhuyenMai = 7500000, HinhAnh="phone.jpg", MoTa="Smartphone mới", LoaiSP="Điện tử" }
+            };
+        }
+        public List<SanPham> GetAll() => _dsSanPham;
+    }
+}
--- core_w1/Program.cs
+++ core_w1/Program.cs
@@ -1,42 +1,54 @@
 using core_w2.Areas.Users.MiddleWares;
 using core_w2.Areas.Users.Services;
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-// Tinsle code
-builder.Services.AddScoped<IRequestLogger, RequestLogger>();
-builder.Services.AddScoped<IUserService, UserService>();
-// Tinsle code end
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace core_w2
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            // Tinsle code
+            builder.Services.AddSingleton<ISanPhamService, SanPhamService>();
+            // Tinsle code
+            builder.Services.AddScoped<IRequestLogger, RequestLogger>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            // Tinsle code end
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
+
+            app.MapControllerRoute(
+                name: "users",
+                pattern: "users/{controller=Home}/{action=Index}/{id?}"
+            );
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(  
-    name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-);
-
-app.MapControllerRoute(
-    name: "users",
-    pattern: "users/{controller=Home}/{action=Index}/{id?}"
-);
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
