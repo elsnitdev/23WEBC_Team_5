c98bde5428d812c6d1daf195c4ec2f0e5c71af22
elsnitdev <tinsle0609@gmail.com>
2025-09-28 10:44:55 +0700

    Admin page + blank

---
+++ core_w1/Areas/Admins/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace core_w2.Areas.Admins.Controllers
+{
+    [Area("Admins")]
+    public class AdminController : Controller
+    {
+      
+        public IActionResult Blank()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
