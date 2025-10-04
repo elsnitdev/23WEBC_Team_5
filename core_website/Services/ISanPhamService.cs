477ffd6efc121f7a9fa302d744f0069916e35c8d
Thương <thuongdozoz25@gmail.com>
2025-10-04 23:15:12 +0700

    Thêm 2 model DonHang và CTDonHang , thêm 1 số chức năng cho ISanPhamService

--- core_website/Services/ISanPhamService.cs
+++ core_website/Services/ISanPhamService.cs
@@ -4,10 +4,22 @@ namespace core_w2.Services
 {
   public interface ISanPhamService
   {
-    List<SanPham> GetAll();
-
-    //Huy code
-    void UpdateList(List<SanPham> list);
+        /// Lấy tất cả sản phẩm
+        List<SanPham> GetAll();
+        /// Lấy sản phẩm theo ID
+        SanPham? GetById(int id);
+        /// Tìm kiếm sản phẩm theo tên (hoặc keyword)
+        List<SanPham> Search(string keyword);
+        /// Thêm sản phẩm mới
+        void Add(SanPham sp);
+        /// Cập nhật thông tin sản phẩm
+        void Update(SanPham sp);
+        /// Xóa sản phẩm theo ID
+        void Delete(int id);
+        /// Lấy top sản phẩm có khuyến mãi
+        List<SanPham> GetKhuyenMai();
+        //Huy code
+        void UpdateList(List<SanPham> list);
     //Huy end
   }
 }
